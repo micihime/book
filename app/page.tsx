8d88443a815f4f7fbfe11fcde1eaddfa7be7018d
Miroslava Hreskova <hroncovam@yahoo.com>
2025-03-31 21:15:40 +0200

    Adding simple header component

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,13 @@ import { Button, Group, Tooltip } from "@mantine/core";
 import { Welcome } from "./components/Welcome/Welcome";
 import { Footer } from "./components/Footer/Footer";
 import styles from "./page.module.css";
+import { Header } from "./components/Header/Header";
 
 export default function HomePage() {
   return (
     <div className={styles.pageContainer}>
+      <Header />
+
       <div className={styles.contentWrap}>
         <Welcome />
         {/* <Text>Welcome to the Forest Adventures!</Text>
